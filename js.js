77c0ac8d989b4fe7042398ba7a7e6c3f8bcc4a63
UKRATITEL <muks121010@gmail.com>
2023-02-12 19:00:46 +0300

    123

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function run() {
-    let htmlCOde = document.getElementById("html-code").value;
-    let cssCOde = document.getElementById("css-code").value;
-    let jsCOde = document.getElementById("js-code").value;
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
     let output = document.getElementById("output");
 
     output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
